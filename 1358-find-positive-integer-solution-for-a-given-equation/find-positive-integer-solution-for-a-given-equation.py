e422b7ebf60d9067f7525b6e5bb342b7bf86eccd
Utsav Thakur <utsavthakur06@gmail.com>
2025-10-04 02:02:51 -0500

    Time: 45 ms (69.85%) | Memory: 17.9 MB (38.74%) - LeetSync

--- 1358-find-positive-integer-solution-for-a-given-equation/find-positive-integer-solution-for-a-given-equation.py
+++ 1358-find-positive-integer-solution-for-a-given-equation/find-positive-integer-solution-for-a-given-equation.py
@@ -11,10 +11,17 @@
 
 class Solution:
     def findSolution(self, customfunction: 'CustomFunction', z: int) -> List[List[int]]:
-        res: list[list[int]] = []
-        for x in range(1, 1001):
-            for y in range(1,1001):
-                if customfunction.f(x,y) == z:
-                    res.append([x,y])
+        res = []
+        x,y = 1,1000
+        while x<=1000 and y>=1:
+            val = customfunction.f(x,y)
+            if val == z:
+                res.append([x,y])
+                x+=1
+                y-=1
+            elif val < z:
+                x+=1
+            else:
+                y-=1
         return res
                     
