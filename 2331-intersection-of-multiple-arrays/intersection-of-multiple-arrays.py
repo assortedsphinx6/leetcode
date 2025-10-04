be4dbc09f742d5bfd23a2bf54fda09c0f3a8c75b
Utsav Thakur <utsavthakur06@gmail.com>
2025-10-04 02:37:34 -0500

    Time: 0 ms (100.00%) | Memory: 18.1 MB (71.76%) - LeetSync

--- 2331-intersection-of-multiple-arrays/intersection-of-multiple-arrays.py
+++ 2331-intersection-of-multiple-arrays/intersection-of-multiple-arrays.py
@@ -1,12 +1,11 @@
 class Solution:
     def intersection(self, nums: List[List[int]]) -> List[int]:
-        d = {}
-        for arr in nums:
-            for x in arr:
-                d[x] = d.get(x,0) + 1
-        res = []
-        for key in d:
-            if d[key] == len(nums):
-                res.append(key)
-        return sorted(res)
+        f=set(nums[0])
+
+        for i in nums[1:]:
+            f=f.intersection(i)
+
+        a=list(f)
+        a.sort()
+        return a
                 
