88e53ce6bf7d9b4c501c7e6ce46573d36357b52f
Utsav Thakur <utsavthakur06@gmail.com>
2025-10-21 18:19:03 -0500

    Time: 3 ms (89.48%) | Memory: 17.8 MB (76.21%) - LeetSync

---
+++ 1353-find-resultant-array-after-removing-anagrams/find-resultant-array-after-removing-anagrams.py
@@ -0,0 +1,11 @@
+class Solution:        
+    def removeAnagrams(self, words: List[str]) -> List[str]:
+        ans = [words[0]]
+        for i in range(1, len(words)):
+            if sorted(words[i-1]) != sorted(words[i]):
+                ans.append(words[i])
+        
+        return ans
+
+        
+
