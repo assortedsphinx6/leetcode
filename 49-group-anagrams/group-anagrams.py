52d664f916439ef2045bd7e76d7f3c41501361e4
Utsav Thakur <utsavthakur06@gmail.com>
2025-10-21 17:59:53 -0500

    Time: 13 ms (55.61%) | Memory: 21.2 MB (47.52%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,5 +1,7 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        if len(strs)==1 or len(strs)==0:
+            return [strs]
         groups = defaultdict(list)
         for s in strs:
             key = tuple(sorted(s)) 
