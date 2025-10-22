d32c0d8b8bad6370e30c5d85da9dbfee1188afab
Utsav Thakur <utsavthakur06@gmail.com>
2025-10-21 21:55:43 -0500

    Time: 75 ms (95.35%) | Memory: 19 MB (26.33%) - LeetSync

--- 1126-minimum-cost-to-connect-sticks/minimum-cost-to-connect-sticks.py
+++ 1126-minimum-cost-to-connect-sticks/minimum-cost-to-connect-sticks.py
@@ -1,8 +1,19 @@
+import heapq
+from typing import List
+
 class Solution:
     def connectSticks(self, sticks: List[int]) -> int:
-        if len(sticks) == 1:
+        if len(sticks) <= 1:
             return 0
-        sticks.sort()
-        x, y = sticks[0], sticks[1]
-        merged = x + y
-        return merged + self.connectSticks([merged] + sticks[2:])
+
+        heapq.heapify(sticks)  # turn into min-heap
+        cost = 0
+
+        while len(sticks) > 1:
+            x = heapq.heappop(sticks)
+            y = heapq.heappop(sticks)
+            new = x + y
+            cost += new
+            heapq.heappush(sticks, new)
+
+        return cost
