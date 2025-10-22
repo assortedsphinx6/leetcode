8633ac734fc6b9181e2cdcf73ccb498603454137
Utsav Thakur <utsavthakur06@gmail.com>
2025-10-22 00:07:30 -0500

    Time: 0 ms (100.00%) | Memory: 17.7 MB (86.77%) - LeetSync

--- 2088-minimum-time-to-type-word-using-special-typewriter/minimum-time-to-type-word-using-special-typewriter.py
+++ 2088-minimum-time-to-type-word-using-special-typewriter/minimum-time-to-type-word-using-special-typewriter.py
@@ -1,11 +1,9 @@
 class Solution:
     def minTimeToType(self, word: str) -> int:
-        count = 0
-        curr = "a"
+        time = 0
+        curr = 'a'
         for ch in word:
             diff = abs(ord(ch) - ord(curr))
-            step = min(diff, 26-diff)
-            count += step + 1
+            time += min(diff, 26 - diff) + 1
             curr = ch
-
-        return count
+        return time
