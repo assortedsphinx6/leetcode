17ba3995aa06a0a38385c65a866e3fe2ab568511
Utsav Thakur <utsavthakur06@gmail.com>
2025-10-21 21:28:29 -0500

    Time: 71 ms (66.14%) | Memory: 18 MB (82.13%) - LeetSync

--- 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.py
+++ 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.py
@@ -1,9 +1,5 @@
 from collections import Counter
+
 class Solution:
     def minSteps(self, s: str, t: str) -> int:
-        freq = [0] * 26
-        for a, b in zip(s, t):
-            freq[ord(a) - 97] += 1
-            freq[ord(b) - 97] -= 1
-        return sum(x for x in freq if x > 0)
-       
+        return sum((Counter(s) - Counter(t)).values())
