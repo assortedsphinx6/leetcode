2e180cade50d38dc39c18ffc8675d83600b90741
Utsav Thakur <utsavthakur06@gmail.com>
2025-10-21 23:52:38 -0500

    Time: 1222 ms (13.87%) | Memory: 31.2 MB (37.67%) - LeetSync

--- 3383-taking-maximum-energy-from-the-mystic-dungeon/taking-maximum-energy-from-the-mystic-dungeon.py
+++ 3383-taking-maximum-energy-from-the-mystic-dungeon/taking-maximum-energy-from-the-mystic-dungeon.py
@@ -1,11 +1,13 @@
 class Solution:
     def maximumEnergy(self, energy: List[int], k: int) -> int:
 
-        dp = [0] * (len(energy) + k) # let dp[i] note the energy we gain starting from index i
+        dp = [0] * len(energy) # let dp[i] note the energy we gain starting from index i
 
         # dp[i] = dp[i+k] + energy[i]
-        for i in range(len(energy)-1,-1,-1):
-            dp[i] = energy[i] + dp[i+k]
+        for i in range(len(dp)-1,-1,-1):
+            if i+k < len(dp):
+                dp[i] = energy[i] + dp[i+k]
+            else:
+                dp[i] = energy[i]
 
-        print (dp)
-        return max(dp[:len(energy)])
+        return max(dp)
