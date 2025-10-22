a37f58fdd1520fab230bfe369df9e5eac9ea253c
Utsav Thakur <utsavthakur06@gmail.com>
2025-10-21 21:26:36 -0500

    Time: 109 ms (51.01%) | Memory: 18 MB (82.13%) - LeetSync

--- 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.py
+++ 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.py
@@ -1,12 +1,9 @@
 from collections import Counter
 class Solution:
     def minSteps(self, s: str, t: str) -> int:
-        c_s = Counter(s)
-        c_t = Counter(t)
-        sorted_t = "".join(sorted(t))
-        sorted_s = "".join(sorted(s))
-        count = sum((c_t - c_s).values())
-        return count
-        count = len(set("practice") - set("leetcode")) # characters in t but not in s need to be added
-        
-        # must account for characters that exist in both s and t but in diff numbers
+        freq = [0] * 26
+        for a, b in zip(s, t):
+            freq[ord(a) - 97] += 1
+            freq[ord(b) - 97] -= 1
+        return sum(x for x in freq if x > 0)
+       
