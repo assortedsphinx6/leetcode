f70d09cae8888bc5aa6f4a87578ba4827de5490f
Utsav Thakur <utsavthakur06@gmail.com>
2025-10-21 19:20:53 -0500

    Time: 110 ms (85.12%) | Memory: 78 MB (43.92%) - LeetSync

---
+++ 146-lru-cache/lru-cache.py
@@ -0,0 +1,22 @@
+from collections import OrderedDict
+
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = OrderedDict()
+
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+        # mark key as recently used
+        self.cache.move_to_end(key)
+        return self.cache[key]
+
+    def put(self, key: int, value: int) -> None:
+        # update if exists
+        if key in self.cache:
+            self.cache.move_to_end(key)
+        self.cache[key] = value
+        # evict least recently used
+        if len(self.cache) > self.capacity:
+            self.cache.popitem(last=False)
