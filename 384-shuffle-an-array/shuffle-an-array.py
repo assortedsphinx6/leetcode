8ca857fe8bdd1f893415948ba26306257f10fcaa
Utsav Thakur <utsavthakur06@gmail.com>
2025-10-21 19:57:09 -0500

    Time: 28 ms (54.95%) | Memory: 22 MB (52.66%) - LeetSync

--- 384-shuffle-an-array/shuffle-an-array.py
+++ 384-shuffle-an-array/shuffle-an-array.py
@@ -1,21 +1,16 @@
+import random
 class Solution:
 
     def __init__(self, nums: List[int]):
         self.nums = nums
-        self.nums_og = nums[:]
-        
+        self.shuffled = self.nums[:]
 
     def reset(self) -> List[int]:
-        return self.nums_og
-        
-
-    def shuffle(self) -> List[int]:
-        n = len(self.nums)
-        for i in range(n - 1, 0, -1):
-                j = random.randint(0, i)  # pick a random index from 0..i
-                self.nums[i], self.nums[j] = self.nums[j], self.nums[i]  # swap
         return self.nums
 
+    def shuffle(self) -> List[int]:
+        random.shuffle(self.shuffled)
+        return self.shuffled
 
 # Your Solution object will be instantiated and called as such:
 # obj = Solution(nums)
